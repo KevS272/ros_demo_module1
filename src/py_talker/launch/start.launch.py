961dfc8aea23d8ca3f07ae3a654f534500ce79a8
Kevin <kevs272@gmail.com>
2023-09-22 16:42:16 +0200

    add foxglove bridge installation and inclusion in launchfile

--- src/py_talker/launch/start.launch.py
+++ src/py_talker/launch/start.launch.py
@@ -1,20 +1,36 @@
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from launch.actions import DeclareLaunchArgument, LogInfo
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+
+    node_talker = Node(
+        package='py_talker',
+        executable='talker',
+        name='talker',
+        output='screen'
+    )
+
+    node_listener = Node(
+        package='py_listener',
+        executable='listener',
+        name='listener',
+        output='screen'
+    )
+
+    foxglove_bridge = IncludeLaunchDescription(
+      XMLLaunchDescriptionSource([os.path.join(
+         get_package_share_directory('foxglove_bridge'), 'launch'),
+         '/foxglove_bridge_launch.xml'])
+      )
+
     return LaunchDescription([
-        Node(
-            package='py_talker',
-            executable='talker',
-            name='talker',
-            output='screen'
-        ),
-        Node(
-            package='py_listener',
-            executable='listener',
-            name='listener',
-            output='screen'
-        ),
+        node_talker,
+        node_listener,
+        foxglove_bridge,
     ])
